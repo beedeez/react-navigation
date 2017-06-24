684f21a79f49262faaadfe23e7f5cd834dd60dd5
Morgan Laupies <morgan.laupies@beedeez.com>
2017-06-24 09:56:19 +0200

    fix should component update

--- src/views/SceneView.js
+++ src/views/SceneView.js
@@ -1,32 +1,52 @@
 /* @flow */
 
-import React, { PureComponent } from 'react';
-import propTypes from 'prop-types';
+import React, { PureComponent } from "react";
+import propTypes from "prop-types";
 
 import type {
   NavigationScreenProp,
   NavigationRoute,
   NavigationAction,
-  NavigationNavigatorProps,
-} from '../TypeDefinition';
+  NavigationNavigatorProps
+} from "../TypeDefinition";
 
 type Props = {
   screenProps?: {},
   navigation: NavigationScreenProp<NavigationRoute, NavigationAction>,
-  component: ReactClass<NavigationNavigatorProps<NavigationRoute>>,
+  component: ReactClass<NavigationNavigatorProps<NavigationRoute>>
 };
 
 export default class SceneView extends PureComponent<void, Props, void> {
   static childContextTypes = {
-    navigation: propTypes.object.isRequired,
+    navigation: propTypes.object.isRequired
   };
 
   props: Props;
 
   getChildContext() {
     return {
-      navigation: this.props.navigation,
+      navigation: this.props.navigation
     };
+  }
+
+  compareState(previousState, nextState) {
+    return (
+      previousState.index === nextState.index &&
+      previousState.isActive === nextState.isActive &&
+      previousState.key === nextState.key &&
+      previousState.routeName === nextState.routeName &&
+      previousState.routes === nextState.routes
+    );
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return (
+      nextProps.screenProps !== this.props.screenProps ||
+      !this.compareState(
+        nextProps.navigation.state,
+        this.props.navigation.state
+      )
+    );
   }
 
   render() {
